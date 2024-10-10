f667cd584b59f705c6205afdfe5d845b5e0fcdac
Gustavo Fernandes <gustavo.mfernandes@fiocruz.br>
2024-10-10 11:10:30 -0300

    Initial Commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SyntaxWeb</title>
+</head>
+<body>
+    BEM VINDO AO SYNTAXWEB !!
+</body>
+</html>
